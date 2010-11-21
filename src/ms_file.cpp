0b7ab1a0fa737dd2f99b044139267098565a9437
Ruediger Meier <ruediger.meier@ga-group.nl>
2010-11-21 07:12:53 +0100

    readFloat, remove original (#if 0-ed) MS code

--- src/ms_file.cpp
+++ src/ms_file.cpp
@@ -104,7 +104,6 @@ float readFloat_IEEE(const char *c, int offset)
 
 float readFloat(const char *c, int offset)
 {
-#if 1
 	union {
 		uint32_t L;
 		float F;
@@ -139,61 +138,6 @@ float readFloat(const char *c, int offset)
 	
 	x.L = ieee_e | ieee_s | ieee_m;
 	return x.F;
-#else
-	const float *src4 = (const float*) (c+offset);
-	float retVal;
-	float *dest4 = &retVal;
-	
-	unsigned char *msbin = (unsigned char *)src4;
-	unsigned char *ieee = (unsigned char *)dest4;
-	unsigned char sign = 0x00;
-	unsigned char ieee_exp = 0x00;
-	int i;
-
-	/* MS Binary Format                         */
-	/* byte order =>    m3 | m2 | m1 | exponent */
-	/* m1 is most significant byte => sbbb|bbbb */
-	/* m3 is the least significant byte         */
-	/*      m = mantissa byte                   */
-	/*      s = sign bit                        */
-	/*      b = bit                             */
-
-	sign = msbin[2] & 0x80;      /* 1000|0000b  */
-
-	/* IEEE Single Precision Float Format       */
-	/*    m3        m2        m1     exponent   */
-	/* mmmm|mmmm mmmm|mmmm emmm|mmmm seee|eeee  */
-	/*          s = sign bit                    */
-	/*          e = exponent bit                */
-	/*          m = mantissa bit                */
-
-	for (i=0; i<4; i++) ieee[i] = 0;
-
-	/* any msbin w/ exponent of zero = zero */
-	if (msbin[3] == 0) return retVal;
-
-	ieee[3] |= sign;
-
-	/* MBF is bias 128 and IEEE is bias 127. ALSO, MBF places   */
-	/* the decimal point before the assumed bit, while          */
-	/* IEEE places the decimal point after the assumed bit.     */
-
-	ieee_exp = msbin[3] - 2;    /* actually, msbin[3]-1-128+127 */
-
-	/* the first 7 bits of the exponent in ieee[3] */
-	ieee[3] |= ieee_exp >> 1;
-
-	/* the one remaining bit in first bin of ieee[2] */
-	ieee[2] |= ieee_exp << 7;
-
-	/* 0111|1111b : mask out the msbin sign bit */
-	ieee[2] |= msbin[2] & 0x7f;
-
-	ieee[1] = msbin[1];
-	ieee[0] = msbin[0];
-
-	return retVal;
-#endif
 }
 
 
