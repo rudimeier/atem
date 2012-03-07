426b1bdcdda7ff3a7f2eb19c56ccffd9fb8cbfc4
Sebastian Freundt <freundt@ga-group.nl>
2012-03-07 13:38:33 +0000

    minor, define default prnt fields statically in the initialiser
    Signed-off-by: Sebastian Freundt <freundt@ga-group.nl>
    
    Conflicts:
    
    	src/metastock.cpp
    
    Signed-off-by: Ruediger Meier <ruediger.meier@ga-group.nl>

--- src/metastock.cpp
+++ src/metastock.cpp
@@ -154,9 +154,9 @@ void FileBuf::resize( int size )
 
 bool Metastock::print_header = true;
 char Metastock::print_sep = '\t';
-unsigned short Metastock::prnt_master_fields = 0;
-unsigned char Metastock::prnt_data_fields = 0;
-unsigned short Metastock::prnt_data_mr_fields = 0;
+unsigned short Metastock::prnt_master_fields = 0xFFFF;
+unsigned char Metastock::prnt_data_fields = 0xFF;
+unsigned short Metastock::prnt_data_mr_fields = M_SYM;
 
 
 Metastock::Metastock() :
